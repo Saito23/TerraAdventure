b681d1e66708ddbea51e31cd6afe48bddf0366ed
Saito23 <belisarioipman@hotmail.com>
2018-10-11 00:41:17 -0400

    contactophp, menucolorhover.

---
+++ contacto/enviar.php
@@ -0,0 +1,11 @@
+<?php 
+    $destino= "experiencias@terraadventure.com.co"
+    $nombre = $_post[nombre];
+    $correo = $_post[correo];
+    $telefono = $_post[telefono];
+    $mensaje = $_post[mensaje];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono "\nMensaje: " . $mensaje;
+    mail($destino,"Contacto", $contenido);
+    header("Location:gracias.html")
+
+?>
